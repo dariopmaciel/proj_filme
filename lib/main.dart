e1c322a1545277103860fc52e78eb9b10bc2ebcf
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-12-10 15:07:16 -0300

    firebse ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:proj_filme/modules/splash/splash_module.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //
+  // final remoteConfig = FirebaseRemoteConfig.instance;
+  // await remoteConfig.fetchAndActivate();
+  //*assim é melhor
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
+
   runApp(const MyApp());
 }
 
--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,14 +1,22 @@
+
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
+
 class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Page'),
+      ),
+      body:  Center(
+        child:
+            // Text('data'),
+            Text(FirebaseRemoteConfig.instance.getString('api_token')),
+      ),
+    );
   }
 }
